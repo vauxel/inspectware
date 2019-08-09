5b14a204ec2a58aa1ba59285ecf1a519b4a311c0
vauxel <vauxel@protonmail.com>
2019-08-09 17:03:56 -0400

    Rough draft of the scheduler widget

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from "express";
+import path from "path";
+import Account from "../models/account";
 const IndexRouter = Router();
 
 IndexRouter.get("/", (req: Request, res: Response) => {
@@ -9,6 +11,20 @@ IndexRouter.get("/", (req: Request, res: Response) => {
 
 IndexRouter.get("/signup", (req: Request, res: Response) => {
 	res.render("signup");
+});
+
+IndexRouter.get("/scheduler/:id", async (req: Request, res: Response) => {
+	let account = await Account.findById(req.params.id);
+
+	if (!account) {
+		res.status(404);
+		res.json({
+			status: "error",
+			message: "An account by that id does not exist"
+		});
+	} else {
+		res.render("scheduler", { accountId: req.params.id, accountName: account.get("name") });
+	}
 });
 
 export default IndexRouter;
